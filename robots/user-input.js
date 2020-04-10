bbaedafde4a9273b4d02c2382dd660bb8866b34d
João Maranhão <joaofelipemaranhao@gmail.com>
2020-04-10 16:18:37 +0000

    feat: add state robot

--- robots/user-input.js
+++ robots/user-input.js
@@ -1,24 +1,27 @@
 const readline = require('readline-sync')
+const state = require('./state')
 
-const content = {}
+const robot = () => {
 
-const askAndReturnSearchTerm = () => {
-    return readline.question('Type a Wikipedia search term: ')
+    const content = {
+    maximumSentences: 7
+    }
+
+    const askAndReturnSearchTerm = () => {
+        return readline.question('Type a Wikipedia search term: ')
+    }
+
+    const askAndReturnPrefix = () => {
+        const prefixes = ['Who is', 'What is', 'The history of']
+        const selectedPrefixIndex = readline.keyInSelect(prefixes, 'Choose one option: ')
+        const selectedPrefixText = prefixes[selectedPrefixIndex]
+
+        return selectedPrefixText
+    }
+
+    content.searchTerm = askAndReturnSearchTerm()
+    content.prefix = askAndReturnPrefix()
+    state.save(content)
 }
 
-const askAndReturnPrefix = () => {
-    const prefixes = ['Who is', 'What is', 'The history of']
-    const selectedPrefixIndex = readline.keyInSelect(prefixes, 'Choose one option: ')
-    const selectedPrefixText = prefixes[selectedPrefixIndex]
-
-    return selectedPrefixText
-}
-
-content.searchTerm = askAndReturnSearchTerm()
-content.prefix = askAndReturnPrefix()
-
-module.exports = {
-    'askAndReturnPrefix': askAndReturnPrefix,
-    'askAndReturnSearchTerm': askAndReturnSearchTerm,
-    'content': content
-}
+module.exports = robot
